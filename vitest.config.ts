a920ed425fbfaebf3b997b09474c2d511d9c4a4c
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-02-13 22:54:40 +0400

    chore: Root cache artifacts configuration was updated

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config'
+import { join } from 'node:path'
 
 export default defineConfig({
+    cacheDir: join(__dirname, 'artifacts/vite/root'),
     test: {
+        server: {
+            deps: {
+                cacheDir: join(__dirname, 'artifacts/vite/root-deps'),
+            },
+        },
         coverage: {
             provider: 'istanbul',
             include: ['packages/*/src/**'],
