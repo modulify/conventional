079d5e8db88e3fc9b7a0a30a7689687b21968f96
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-02-13 22:54:31 +0400

    chore(conventional-changelog): Vite and Vitest cache paths were moved to artifacts

--- packages/conventional-changelog/vite.config.common.ts
+++ packages/conventional-changelog/vite.config.common.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { join } from 'node:path'
 
 export default defineConfig({
+  cacheDir: join(__dirname, '../../artifacts/vite/conventional-changelog'),
   resolve: {
     alias: {
       '@': join(__dirname, './src/'),
--- packages/conventional-changelog/vitest.config.ts
+++ packages/conventional-changelog/vitest.config.ts
@@ -2,11 +2,17 @@ import {
   defineConfig,
   mergeConfig,
 } from 'vitest/config'
+import { join } from 'node:path'
 
 import common from './vite.config.common'
 
 export default mergeConfig(common, defineConfig({
   test: {
+    server: {
+      deps: {
+        cacheDir: join(__dirname, '../../artifacts/vite/conventional-changelog-deps'),
+      },
+    },
     coverage: {
       provider: 'istanbul',
       include: ['src/**'],
