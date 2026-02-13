e70950347169ca571f3290711dce3eb643a88b89
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-02-13 22:54:35 +0400

    chore(conventional-git): Vite and Vitest cache paths were moved to artifacts

--- packages/conventional-git/vite.config.common.ts
+++ packages/conventional-git/vite.config.common.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { join } from 'node:path'
 
 export default defineConfig({
+  cacheDir: join(__dirname, '../../artifacts/vite/conventional-git'),
   resolve: {
     alias: {
       '@': join(__dirname, './src/'),
